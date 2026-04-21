405577e231482a73b8e2f351b2491dd508d638bf
WillLandon <willlandon350@gmail.com>
2026-04-21 12:24:55 -0400

    done

--- template/Driver.java
+++ template/Driver.java
@@ -8,25 +8,30 @@ public class Driver {
 
 	public static void TEST_CITIES() {
 	
-		//TODO: 
-			//Create Vertices
-	 
-		//TODO:
-			//Create Edges
-			//	hint - build the edge by adding a neighbor to a node
-		
-		//TODO:
-			//Create an instance of a new GraphUtils<String>();
-			//invoke the pathExists method of your GraphUtils & print the result for each of the following routes:
-		
-			//Austin -> Chicago
-			//Denver -> Austin
-			//Houston -> Dallas
-			//Austin -> Atlanta
-			//Denver -> Dallas
-			//Washington -> Houston
-		
+		GraphNode<String> austin     = new GraphNode<>("Austin");
+        GraphNode<String> chicago    = new GraphNode<>("Chicago");
+        GraphNode<String> atlanta    = new GraphNode<>("Atlanta");
+        GraphNode<String> houston    = new GraphNode<>("Houston");
+        GraphNode<String> washington = new GraphNode<>("Washington");
+        GraphNode<String> denver     = new GraphNode<>("Denver");
+        GraphNode<String> dallas     = new GraphNode<>("Dallas");
 
+        austin.addneighbours(chicago);
+        austin.addneighbours(atlanta);
+        washington.addneighbours(houston);
+        washington.addneighbours(austin);
+
+        GraphUtils<String> utils = new GraphUtils<>();
+
+        System.out.println("Routes Available:");
+        System.out.println("--------------------------------");
+        System.out.printf("Austin -> Chicago:     %b%n",  utils.pathExists(austin,     chicago));
+        System.out.printf("Denver -> Austin:      %b%n",  utils.pathExists(denver,     austin));
+        System.out.printf("Houston -> Dallas:     %b%n",  utils.pathExists(houston,    dallas));
+        System.out.printf("Austin -> Atlanta:     %b%n",  utils.pathExists(austin,     atlanta));
+        System.out.printf("Denver -> Dallas:      %b%n",  utils.pathExists(denver,     dallas));
+        System.out.printf("Washington -> Houston: %b%n",  utils.pathExists(washington, houston));
+	
 	}
 	
 	/** Expected results:
--- template/GraphUtils.java
+++ template/GraphUtils.java
@@ -1,26 +1,40 @@
 package template;
-  
- 
+
+import java.util.LinkedList;
+import java.util.HashSet;
 
 public class GraphUtils<T> {
 
-	public Boolean pathExists(GraphNode<T> targetFromNode, GraphNode<T> targetToNode) {
- 
-		//TODO:
+    public Boolean pathExists(GraphNode<T> targetFromNode, GraphNode<T> targetToNode) {
 
-		// create queue to manage nodes
-		// create set to manage visitedNodes
+        //TODO:
+        // create queue to manage nodes
+        LinkedList<GraphNode<T>> queue = new LinkedList<GraphNode<T>>();
+        // create set to manage visitedNodes
+        HashSet<GraphNode<T>> visited = new HashSet<GraphNode<T>>();
 
-		//start from the targetFromNode
-			
-			//for all neighbors:
-				//check if visited.  If not, add to the queue.  
-				//if targetToNode has been visited, return true
-			
-			
-		
-		//if u get here
-		return false;
-	}
+        //start from the targetFromNode
+        queue.add(targetFromNode);
+        visited.add(targetFromNode);
 
+        while (!queue.isEmpty()) {
+            GraphNode<T> node = queue.poll();
+
+            //for all neighbors:
+            for (GraphNode<T> neighbour : node.getNeighbours()) {
+                //check if visited.  If not, add to the queue.  
+                if (!visited.contains(neighbour)) {
+                    visited.add(neighbour);
+                    queue.add(neighbour);
+                }
+                //if targetToNode has been visited, return true
+                if (neighbour == targetToNode) {
+                    return true;
+                }
+            }
+        }
+
+        //if u get here
+        return false;
+    }
 }
